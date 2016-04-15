c90810c9facd76238b7e0e5bab031ddfb895a0b2
David Block <dave@northcreek.ca>
2016-04-15 14:37:11 -0600

    moved user picture location

--- v2/plum/resources/views/sidebar.blade.php
+++ v2/plum/resources/views/sidebar.blade.php
@@ -7,7 +7,7 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel">
       <div class="pull-left image">
-        <img src="{{ asset("/bower_components/admin-lte/dist/img/LisaGregory.jpg") }}" class="img-circle" alt="User Image">
+        <img src="{{ asset("/images/LisaGregory.jpg") }}" class="img-circle" alt="User Image">
       </div>
       <div class="pull-left info">
         <p>Lisa Gregory</p>
