8933c4ca76bc1abeaac5d580a13f075108ef68f2
David Block <dave@northcreek.ca>
2016-06-09 18:03:41 -0600

    emails going to various places when form uploaded and interview completed

---
+++ v2/plum/resources/views/email/form_uploaded.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html" http-equiv="content-type">
+  </head>
+  <body>
+    <p>Candidate Form Uploaded:</p>
+    <p>{{$candidateName}}</p>
+    <p>{{$candidateID}}</p>
+    <p>Time of Form Upload: {{$date}}</p>
+
+  </body>
+</html>
---
+++ v2/plum/resources/views/email/interview_complete.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html" http-equiv="content-type">
+  </head>
+  <body>
+    <p>Candidate Interview Complete:</p>
+    <p>{{$candidateName}}</p>
+    <p>{{$candidateID}}</p>
+    <p>Time of Data Upload: {{$date}}</p>
+    <p>Uploaded by: {{$consultantName}}<br>
+    </p>
+    
+  </body>
+</html>
